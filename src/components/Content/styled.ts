fdcecf6a9e9c6b8318caa83125be6e3e66e72e75
Felipe Freire <fellipefreiire3@gmail.com>
2021-04-13 21:57:43 -0300

    scrollbar css

--- src/components/Content/styled.ts
+++ src/components/Content/styled.ts
@@ -5,4 +5,19 @@ export const Container = styled.div`
   color: ${props => props.theme.dark.colors.white};
   background-color: ${props => props.theme.dark.colors.primary};
   padding: 1.5rem 2rem;
+  height: calc(100vh - 5rem);
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 1.5rem;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.dark.colors.secondary};
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => theme.dark.colors.tertiary};
+  }
 `
