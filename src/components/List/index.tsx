c666da582847a5890af48bb587b83d6d22121f24
Felipe Freire <fellipefreiire3@gmail.com>
2021-04-13 22:33:56 -0300

    list filter

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -4,90 +4,35 @@ import SelectInput from '../SelectInput'
 import * as S from './styled'
 
 const List: React.FC = (): JSX.Element => {
-  const options = [
-    { value: 'Rodrigo', label: 'Rodrigo' },
-    { value: 'Felipe', label: 'Felipe' },
-    { value: 'Ana', label: 'Ana' }
+  const months = [
+    { value: 7, label: 'Julho' },
+    { value: 8, label: 'Agosto' },
+    { value: 9, label: 'Setembro' }
+  ]
+
+  const years = [
+    { value: 2021, label: 2021 },
+    { value: 2020, label: 2020 },
+    { value: 2019, label: 2019 }
   ]
   return (
     <S.Container>
       <ContentHeader title='Saídas' lineColor='warning'>
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
 
+      <S.Filters>
+        <S.Button type='button' className='tag-filter-recurrent'>
+          Recorrentes
+        </S.Button>
+
+        <S.Button type='button' className='tag-filter-eventual'>
+          Eventuais
+        </S.Button>
+      </S.Filters>
+
       <S.Content>
-        <HistoryFinanceCard
-          tagColor='warning'
-          title='Conta de luz'
-          subtitle='13/04/2021'
-          amount='R$ 130,00'
-        />
-        <HistoryFinanceCard
-          tagColor='warning'
-          title='Conta de luz'
-          subtitle='13/04/2021'
-          amount='R$ 130,00'
-        />
-        <HistoryFinanceCard
-          tagColor='warning'
-          title='Conta de luz'
-          subtitle='13/04/2021'
-          amount='R$ 130,00'
-        />
-        <HistoryFinanceCard
-          tagColor='warning'
-          title='Conta de luz'
-          subtitle='13/04/2021'
-          amount='R$ 130,00'
-        />
-        <HistoryFinanceCard
-          tagColor='warning'
-          title='Conta de luz'
-          subtitle='13/04/2021'
-          amount='R$ 130,00'
-        />
-        <HistoryFinanceCard
-          tagColor='warning'
-          title='Conta de luz'
-          subtitle='13/04/2021'
-          amount='R$ 130,00'
-        />
-        <HistoryFinanceCard
-          tagColor='warning'
-          title='Conta de luz'
-          subtitle='13/04/2021'
-          amount='R$ 130,00'
-        />
-        <HistoryFinanceCard
-          tagColor='warning'
-          title='Conta de luz'
-          subtitle='13/04/2021'
-          amount='R$ 130,00'
-        />
-        <HistoryFinanceCard
-          tagColor='warning'
-          title='Conta de luz'
-          subtitle='13/04/2021'
-          amount='R$ 130,00'
-        />
-        <HistoryFinanceCard
-          tagColor='warning'
-          title='Conta de luz'
-          subtitle='13/04/2021'
-          amount='R$ 130,00'
-        />
-        <HistoryFinanceCard
-          tagColor='warning'
-          title='Conta de luz'
-          subtitle='13/04/2021'
-          amount='R$ 130,00'
-        />
-        <HistoryFinanceCard
-          tagColor='warning'
-          title='Conta de luz'
-          subtitle='13/04/2021'
-          amount='R$ 130,00'
-        />
         <HistoryFinanceCard
           tagColor='warning'
           title='Conta de luz'
--- src/components/List/styled.ts
+++ src/components/List/styled.ts
@@ -1,5 +1,51 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Container = styled.div``
 
 export const Content = styled.div``
+
+export const Filters = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin-bottom: 2rem;
+`
+
+export const Button = styled.button`
+  font-size: 18px;
+  font-weight: 600;
+  background: none;
+  color: ${({ theme }) => theme.dark.colors.white};
+  margin: 0 0.5rem;
+  transition: opacity 0.5s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  ${({ className }) =>
+    className === 'tag-filter-recurrent' &&
+    css`
+      &:after {
+        content: '';
+        display: block;
+        width: 3.5rem;
+        padding-bottom: 0.25rem;
+        margin: 0 auto;
+        border-bottom: 10px solid ${({ theme }) => theme.dark.colors.success};
+      }
+    `}
+
+  ${({ className }) =>
+    className === 'tag-filter-eventual' &&
+    css`
+      &:after {
+        content: '';
+        display: block;
+        width: 3.5rem;
+        padding-bottom: 0.25rem;
+        margin: 0 auto;
+        border-bottom: 10px solid ${({ theme }) => theme.dark.colors.warning};
+      }
+    `}
+`
