b1911125bd985913206e0f3bea067a1ed0886f01
Felipe Freire <fellipefreiire3@gmail.com>
2021-04-16 21:51:03 -0300

    auth fix

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,6 +14,7 @@ export default createGlobalStyle`
   }
 
   body {
+    background-color: ${({ theme }) => theme.colors.primary};
     font: 400 16px Roboto, sans-serif;
     color: ${({ theme }) => theme.colors.white}
   }
